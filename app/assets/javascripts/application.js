1353a58dc9d589a06c78a7bbe9d68938967ecf24
奥村彰良 <aonk13965@gmail.com>
2019-11-01 20:22:03 +0900

    add chart.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,48 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require Chart.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+window.draw_graph = function() {
+    var barNum, bdColors, bgColors, ctx, i, j, labels, myChart, ref;
+    ctx = document.getElementById("myChart").getContext('2d');
+    barNum = 6;
+    labels = new Array(barNum);
+    bgColors = new Array(barNum);
+    bdColors = new Array(barNum);
+    for (i = j = 0, ref = barNum; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
+        labels[i] = 'data' + i;
+        bgColors[i] = 'rgba(75, 192, 192, 0.2)';
+        bdColors[i] = 'rgba(75, 192, 192, 1)';
+    }
+    return myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            datasets: [{
+                label: '# of Votes',
+                data: gon.bardata,
+                backgroundColor: bgColors,
+                borderColor: bdColors,
+                borderWidth: 1
+            }, {
+                label: 'Line Dataset',
+                data: gon.linedata,
+                type: 'line'
+            }],
+            labels: labels
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+};
