4e6c5d52ab6c1a4feb1b310fe8d30814a75f06ce
terasaka187 <billionsystem187@gmail.com>
2019-11-02 16:03:28 +0900

    コンフリクト解消かくにん

--- db/migrate/20191101142131_create_hospitals.rb
+++ db/migrate/20191101142131_create_hospitals.rb
@@ -3,7 +3,7 @@ class CreateHospitals < ActiveRecord::Migration[5.2]
     create_table :hospitals do |t|
       t.string :name
       t.string :address
-      t.integer :doctors_id #医者ID
+
       t.timestamps
     end
   end
