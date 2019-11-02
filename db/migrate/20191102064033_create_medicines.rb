a5400d964cdb11a8f5f9892cee0a7d4515f566db
平林 宏淳 <hh@MacBook-Pro.local>
2019-11-02 17:34:40 +0900

    rename add mdeical_model

---
+++ db/migrate/20191102064033_create_medicines.rb
@@ -0,0 +1,9 @@
+class CreateMdeicines < ActiveRecord::Migration[5.2]
+  def change
+    create_table :medicines do |t|
+      t.string :name
+      t.string :description
+      t.timestamps
+    end
+  end
+end
