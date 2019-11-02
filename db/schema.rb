020b8cd7676c142eb2754eca5461274c2376e4db
奥村彰良 <aonk13965@gmail.com>
2019-11-02 16:40:39 +0900

    modify column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_055140) do
+ActiveRecord::Schema.define(version: 2019_11_01_142131) do
 
   create_table "doctors", force: :cascade do |t|
     t.string "name"
@@ -21,15 +21,34 @@ ActiveRecord::Schema.define(version: 2019_11_01_055140) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "hospitals", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "medical_examinations", force: :cascade do |t|
     t.float "weight"
-    t.float "temp"
-    t.integer "SYS"
-    t.integer "DIA"
+    t.float "body_temp"
+    t.integer "systolic_bp"
+    t.integer "diastorlic_bp"
     t.text "memo"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
