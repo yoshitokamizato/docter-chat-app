dbc26af65710fa838143e49117c380c52bb70cee
terasaka187 <billionsystem187@gmail.com>
2019-11-08 22:33:29 +0900

    クラスは定義できたんですが、

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,17 +1,2 @@
 class Hospital < ApplicationRecord
-
-    def self.import(path)
-        list = []
-        CSV.foreach(path, headers: true) do |row|
-            list << {
-                name: row["name"],
-                address: row["address"],
-                phone: row["phone"]
-            }
-        end
-        puts "インポート開始"
-        Hospital.create!(list)
-        puts "インポートに成功しました"
-    rescue ActiveModel::UnknownAttributeError => invalid
-    end
 end
---
+++ app/models/import_csv.rb
@@ -0,0 +1,17 @@
+class  import_csv < ApplicationRecord
+
+    def self.import(path)
+        list = []
+        CSV.foreach(path, headers: true) do |row|
+            list << {
+                name: row["name"],
+                address: row["address"],
+                phone: row["phone"]
+            }
+        end
+        puts "インポート開始"
+        Hospital.create!(list)
+        puts "インポートに成功しました"
+    rescue ActiveModel::UnknownAttributeError => invalid
+    end
+end
