e08643b3742a13d14dc66683bcd9fff03276521a
平林宏淳 <hh@MacBook-Pro.local>
2019-11-18 22:27:15 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_144449) do
+
+ActiveRecord::Schema.define(version: 2019_11_05_042100) do
+
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
