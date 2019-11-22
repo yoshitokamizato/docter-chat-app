da814c367274ce13e52bc47127b5ff4fda047893
平林宏淳 <hh@MacBook-Pro.local>
2019-11-23 01:18:18 +0900

    外部キー追加　

--- db/migrate/20191105042100_create_user_medicines.rb
+++ db/migrate/20191105042100_create_user_medicines.rb
@@ -1,7 +1,7 @@
 class CreateUserMedicines < ActiveRecord::Migration[5.2]
   def change
     create_table :user_medicines do |t|
-      t.references :medicine
+      t.references :medicine, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
     end
